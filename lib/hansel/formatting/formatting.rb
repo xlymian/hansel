1f56160e3b56668047e9858fb6e2b7b30ae96a44
Paul Mylchreest <paul.mylchreest@mac.com>
2010-06-19 21:05:26 -0400

    Fixed bug with octave png file name.

--- lib/hansel/formatting/formatting.rb
+++ lib/hansel/formatting/formatting.rb
@@ -18,7 +18,11 @@ module HanselCore
       file_name = output_filename{ "-#{num_conns.to_s}" }
       template = opts.template || File.join( [ File.dirname(__FILE__), '../../..', opts.template_path, 'octave.m.erb' ] )
       File.open(file_name, "w+") do |file|
-        file.puts OctaveFormatter.new(res, { :output_file_name => file_name, :template => template }).format
+        file.puts OctaveFormatter.new(res,
+            { :output_file_name => file_name,
+              :template         => template,
+              :png_file_name    => "#{@server}:#{@port}-#{num_conns.to_s}.png"
+            }).format
       end
     end
 
@@ -36,8 +40,8 @@ module HanselCore
     def output_filename
       opts, part = options, (block_given? ? yield : '')
       type    = { :yaml => 'yml', :csv => 'csv', :octave => 'm' }[opts.format.to_sym]
-      server, port = (res = results.first) && res.server, res.port
-      [File.join([opts.output_dir, ("#{server}:#{port}" + part)]), type].join('.')
+      @server, @port = (res = results.first) && res.server, res.port
+      [File.join([opts.output_dir, ("#{@server}:#{@port}" + part)]), type].join('.')
     end
   end
 end
--- lib/hansel/formatting/octave_formatter.rb
+++ lib/hansel/formatting/octave_formatter.rb
@@ -8,15 +8,15 @@ module HanselCore
     def initialize(data, options = {})
       @data       = data
       @template   = options[:template]
-      @rates      = @data.map(&:rate)
+      @rates      = @data.map &:rate
       @max_rate   = @rates.max
-      @png_output = [(options[:output_file_name] || 'stats'), 'png'].join('.')
+      @png_output = options[:png_file_name]
     end
 
     # For each value of rate, collate the values in each variable in @vars
     # and format using the erb template
     def format
-      ERB.new(File.read(@template)).result(binding)
+      ERB.new(File.read(@template)).result binding
     end
   end
 end
